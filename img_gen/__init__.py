4a38de3221a5b4eb61b3641bffd926cb08a7d991
Semen Cherezov <semen.nexus@gmail.com>
2020-12-27 12:54:22 +0200

    add img_gen lib

---
+++ img_gen/__init__.py
@@ -0,0 +1,64 @@
+from PIL import Image, ImageDraw, ImageFont
+
+class StrokeCenterDrawer:
+    def __init__(self, img: Image, font: str):
+        self._draw = ImageDraw.Draw(img)
+
+        font_size = img.width // 13
+        self._font = ImageFont.truetype(font, size=font_size)
+
+        self._generate_draw_positions(img)
+
+    def _generate_draw_positions(self, img: Image):
+        print("_generate_draw_positions")
+        x_center, y_center = img.width // 2, img.height // 2
+        self._center_position = (x_center, y_center)
+        shift = (img.width // 13) / 50
+        self._stroke_positions = [
+            (x_center - shift, y_center),
+            (x_center - shift, y_center + shift),
+            (x_center, y_center + shift),
+            (x_center + shift, y_center + shift),
+            (x_center + shift, y_center),
+            (x_center + shift, y_center - shift),
+            (x_center, y_center - shift),
+            (x_center - shift, y_center - shift),
+        ]
+
+    def _draw_pos(self, pos, text: str, color):
+        self._draw.text(pos, text, font=self._font, fill=color, anchor='ms', align="center")
+
+    def draw(self, text: str, main_color = (255, 255, 255), stroke_color = (0, 0, 0)):
+        print("draw")
+        for pos in self._stroke_positions:
+            self._draw_pos(pos, text, color=stroke_color)
+        self._draw_pos(self._center_position, text, color=main_color)
+
+
+# def _texting(img, text: str):
+#     y = img.height // 2
+#     x = img.width // 2
+#     shadowColor = "black"
+#     # for adj in range(outlineAmount):
+#     adj = font_size / 50
+#     _draw_text(x - adj, y, text,
+#                shadowColor)
+#     _draw_text(x + adj, y, text,
+#                shadowColor)
+#     _draw_text(x, y + adj, text, shadowColor)
+#     # move down
+#     _draw_text(x, y - adj, text,
+#                shadowColor)
+#     _draw_text(x - adj, y + adj, text, shadowColor)
+#     _draw_text(x + adj, y + adj, text,
+#                shadowColor)
+#     _draw_text(x - adj, y - adj, text,
+#                shadowColor)
+#     _draw_text(x + adj, y - adj, text,
+#                shadowColor)
+#     _draw_text(x, y, text, (255, 255, 255))
+
+def generate_prediction(img_path: str, font: str, prediction: str) -> Image:
+    img = Image.open(img_path)
+    StrokeCenterDrawer(img, font).draw(prediction)
+    return img
--- pillow_check.py
+++ pillow_check.py
@@ -1,11 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 import sys
-pictures = ["alexey-savchenko-nGDZqViSOHQ-unsplash.jpg","tina-vanhove-lOP9D8VN7Ew-unsplash.jpg","alexander-sergienko-y-5CJNSwV-E-unsplash.jpg"  ]
+import csv
+
+pictures = ["alexey-savchenko-nGDZqViSOHQ-unsplash.jpg", "tina-vanhove-lOP9D8VN7Ew-unsplash.jpg",
+            "alexander-sergienko-y-5CJNSwV-E-unsplash.jpg"]
 img = Image.open(random.choice(pictures))
 draw = ImageDraw.Draw(img)
-font_size = img.width//10
+font_size = img.width // 13
 font = ImageFont.truetype("arial.ttf", size=font_size)
+
+predictions =[]
+# predictions = ["Счастье уже \n стоит за дверью.", "Будьте внимательны \n к своему здоровью.",
+#      "Прислушайтесь к \n советам интуиции.", "Люди, что \n сейчас рядом,\n будут помогать\n весь год.",
+#      "Нужная встреча\n произойдет\n совсем скоро.", "Любовь улыбается\n и ждет своего часа.",
+#      "Везение в \nденежных вопросах.", "Удача в \nлюбом начинании.",
+#       "Счастье где-то рядом,\n обернитесь вокруг.", "Год слёз,\n но только от радости.",
+#       "Вас ожидает поездка\n в новую страну.", "Этот год кардинально\n изменит Вашу жизнь.",
+#       "Возможно пополнение\n в семье в\n этом году.", "Карьерный рост будет\n стремителен и успешен.",
+#       "Мир и покой\n в семье весь год.", "Романтическое свидание\n поможет найти\n взаимные чувства.",
+#       "Любое дело \nобречено на успех.",
+#      "Путешествие для души \nподарит необходимый отдых\n и новые впечатления.",
+#       "Откройте сердце любви\n в этом году.", "Семья подарит \nнастоящую поддержку.",
+#       "В этом году ожидается \nмножество новых знакомств.", "Пора отдохнуть \nи заняться хобби.",
+#     "В этом году\n Вас ждет море счастья.", "Фортуна ответит\n <да> на любой вопрос.",
+#     "Рискуйте\n и обязательно выиграете.", "Этот год принесет\n повышение на работе.",
+#    "Желаемое осуществится\n в середине года.", "Этот год принесет\n только счастье и успех.",
+#   "В этом году\n к Вам придет любовь,\n не упустите ее.",
+#  "Очень удачный год\n для начала \nсобственного бизнеса."]
 
 
 def draw_text(x, y, text, color):
@@ -13,7 +35,6 @@ def draw_text(x, y, text, color):
 
 
 def texting(text: str):
-    outlineAmount = 3
     y = img.height // 2
     x = img.width // 2
     shadowColor = "black"
@@ -42,7 +63,24 @@ def texting(text: str):
     draw_text(x, y, text, (255, 255, 255))  # draw.text((x, y), text, font=font, fill=(255, 255, 255), anchor='ms')
 
 
+def csv_reader(file_obj):
+    """
+    Read a CSV file using csv.DictReader
+    """
+    reader = csv.reader(file_obj, delimiter=';')
+    for line in reader:
+        predictions.append(line[0])
+
+
+
 if __name__ == '__main__':
-    texting("Это твоё\n предсказание")
+    csv_path = "2021.csv"
+    with open(csv_path, "r", encoding='utf-8') as f_obj:
+        csv_reader(f_obj)
+    letter = random.choice(predictions)
+    texting(letter)
+    size = font.getsize(letter)
+    print(size)
     img.show()
 # img = Image.new('RGBA', (300, 300), image)
+#
