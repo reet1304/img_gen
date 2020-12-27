c5630a32e6393576ea436b3089243ed222ef94f3
Илья <lionson@mail.ru>
2020-12-27 12:20:44 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='img_gen',
+    version='0.0.4',
+    install_requires=[
+          'pillow',
+    ],
+    packages=find_packages(),
+)
