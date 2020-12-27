c67eacd46c1de9e8ef50edd43bd952f4cd9c4bd0
Semen Cherezov <semen.nexus@gmail.com>
2020-12-27 11:01:38 +0200

    font based on size (+random pictures)

--- pillow_check.py
+++ pillow_check.py
@@ -1,9 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
+import random
 import sys
-
-img = Image.open('tina-vanhove-lOP9D8VN7Ew-unsplash.jpg')
+pictures = ["alexey-savchenko-nGDZqViSOHQ-unsplash.jpg","tina-vanhove-lOP9D8VN7Ew-unsplash.jpg","alexander-sergienko-y-5CJNSwV-E-unsplash.jpg"  ]
+img = Image.open(random.choice(pictures))
 draw = ImageDraw.Draw(img)
-font_size = 200
+font_size = img.width//10
 font = ImageFont.truetype("arial.ttf", size=font_size)
 
 
@@ -19,31 +20,26 @@ def texting(text: str):
     # for adj in range(outlineAmount):
     adj = font_size / 50
     # move right
-    draw_text(x - adj, y, text, shadowColor)
-    # draw.text((x - adj, y), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x - adj, y, text, shadowColor)  # draw.text((x - adj, y), text, font=font, fill=shadowColor, anchor='ms')
     # move left
-    draw_text(x + adj, y, text, shadowColor)
-    # draw.text((x + adj, y), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x + adj, y, text, shadowColor)  # draw.text((x + adj, y), text, font=font, fill=shadowColor, anchor='ms')
     # move up
-    draw_text(x, y + adj, text, shadowColor)
-    #  draw.text((x, y + adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x, y + adj, text, shadowColor)  # draw.text((x, y + adj), text, font=font, fill=shadowColor, anchor='ms')
     # move down
-    draw_text(x, y - adj, text, shadowColor)
-    # draw.text((x, y - adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x, y - adj, text, shadowColor)  # draw.text((x, y - adj), text, font=font, fill=shadowColor, anchor='ms')
     # diagnal left up
-    draw_text(x - adj, y + adj, text, shadowColor)
-    # draw.text((x - adj, y + adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x - adj, y + adj, text,
+              shadowColor)  # draw.text((x - adj, y + adj), text, font=font, fill=shadowColor, anchor='ms')
     # diagnal right up
-    draw_text(x + adj, y + adj, text, shadowColor)
-    # draw.text((x + adj, y + adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x + adj, y + adj, text,
+              shadowColor)  # draw.text((x + adj, y + adj), text, font=font, fill=shadowColor, anchor='ms')
     # diagnal left down
-    draw_text(x - adj, y - adj, text, shadowColor)
-    # draw.text((x - adj, y - adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x - adj, y - adj, text,
+              shadowColor)  # draw.text((x - adj, y - adj), text, font=font, fill=shadowColor, anchor='ms')
     # diagnal right down
-    draw_text(x + adj, y - adj, text, shadowColor)
-    # draw.text((x + adj, y - adj), text, font=font, fill=shadowColor, anchor='ms')
-    draw_text(x, y, text, (255,255,255))
-    # draw.text((x, y), text, font=font, fill=(255, 255, 255), anchor='ms')
+    draw_text(x + adj, y - adj, text,
+              shadowColor)  # draw.text((x + adj, y - adj), text, font=font, fill=shadowColor, anchor='ms')
+    draw_text(x, y, text, (255, 255, 255))  # draw.text((x, y), text, font=font, fill=(255, 255, 255), anchor='ms')
 
 
 if __name__ == '__main__':
