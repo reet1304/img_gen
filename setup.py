bd352347fb0354ee780807d9f009d0b3bf527fb8
Илья <lionson@mail.ru>
2020-12-29 22:32:15 +0200

    restruct

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='img_gen',
-    version='0.0.10',
+    version='0.0.11',
     install_requires=[
           'pillow',
     ],
     packages=find_packages(),
     scripts=[
-        'img_gen_preshow'
+        'img_gen/bin/img_gen_preshow'
     ],
-    package_data={'img_gen': ['arial.ttf']},
+    package_data={'img_gen': ['fonts/arial.ttf']},
 
 )
